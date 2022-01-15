4d7460fa1d73a7f36a76651e911bc516eccf3d2c
CodeForNilesh <patilnileshg@gmail.com>
2022-01-15 12:50:52 +0530

    Table and Stored Procedure Change
    1] Added IsActive in Persons table
    2] Added check for IsActive in usp_GetEntireWorkflow.

--- Workflow.StoredProcedures.sql
+++ Workflow.StoredProcedures.sql
@@ -1,8 +1,12 @@
 
 
-
-
-/****** Object:  StoredProcedure [dbo].[usp_GetEntireWorkflow]    Script Date: 15-01-2022 01:52:51 ******/
+/****** Object:  StoredProcedure [dbo].[usp_GetWorkflowMeta]    Script Date: 15-01-2022 12:07:23 ******/
+DROP PROCEDURE IF EXISTS [dbo].[usp_GetWorkflowMeta]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_GetEntireWorkflow]    Script Date: 15-01-2022 12:07:23 ******/
+DROP PROCEDURE IF EXISTS [dbo].[usp_GetEntireWorkflow]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_GetEntireWorkflow]    Script Date: 15-01-2022 12:07:23 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -18,29 +22,29 @@ AS
 BEGIN
     SET NOCOUNT ON
 	
-	SELECT * FROM WORKFLOW WHERE WorkflowID = @WorkflowID
+	SELECT WorkflowID, Title, Details FROM WORKFLOW WHERE WorkflowID = @WorkflowID
 
-	SELECT * FROM STAGE WHERE WorkflowID = @WorkflowID
+	SELECT StageID, Title, Description, ActionCommentMandetory, ReviewTime, EscalationTime, IsAnyApprover FROM STAGE WHERE WorkflowID = @WorkflowID
 
-	SELECT * FROM STAGE ST LEFT JOIN STAGEREVIEWERS SR ON ST.StageID = SR.StageID
-	LEFT JOIN PERSONS PS ON SR.ReviewerID = PS.UserID 
+	SELECT PS.UserID, PS.FirstName, PS.MiddleName, PS.LastName, PS.Email, PS.Mobile, PS.City, PS.[State], PS.District, PS.Pincode FROM STAGE ST INNER JOIN STAGEREVIEWERS SR ON ST.StageID = SR.StageID
+	INNER JOIN PERSONS PS ON SR.ReviewerID = PS.UserID 
 	WHERE ST.WorkflowID = @WorkflowID
 
-	SELECT * FROM STAGE ST LEFT JOIN STAGEREVIEWERS SR ON ST.StageID = SR.StageID
-	LEFT JOIN DEPARTMENT DT ON SR.DepartmentID = DT.DepartmentID 
+	SELECT DT.DepartmentID, DT.DepartmentName, DT.City, DT.[State], DT.District, DT.Pincode, DT.Latitude, DT.Longitude, DT.DeptOwner, DT.ContactDetails FROM STAGE ST INNER JOIN STAGEREVIEWERS SR ON ST.StageID = SR.StageID
+	INNER JOIN DEPARTMENT DT ON SR.DepartmentID = DT.DepartmentID 
 	WHERE ST.WorkflowID = @WorkflowID
 
-	SELECT * FROM STAGE ST LEFT JOIN STAGEESCALATORS SE ON ST.StageID = SE.StageID
-	LEFT JOIN PERSONS PS ON SE.EscalatorID = PS.UserID 
-	WHERE ST.WorkflowID = @WorkflowID
+	SELECT PS.UserID, PS.FirstName, PS.MiddleName, PS.LastName, PS.Email, PS.Mobile, PS.City, PS.[State], PS.District, PS.Pincode FROM STAGE ST INNER JOIN STAGEESCALATORS SE ON ST.StageID = SE.StageID
+	INNER JOIN PERSONS PS ON SE.EscalatorID = PS.UserID 
+	WHERE ST.WorkflowID = @WorkflowID AND PS.IsActive = 1
 
-	SELECT * FROM STAGE ST LEFT JOIN STAGEESCALATORS SE ON ST.StageID = SE.StageID
-	LEFT JOIN DEPARTMENT DT ON SE.DepartmentID = DT.DepartmentID 
+	SELECT DT.DepartmentID, DT.DepartmentName, DT.City, DT.[State], DT.District, DT.Pincode, DT.Latitude, DT.Longitude, DT.DeptOwner, DT.ContactDetails FROM STAGE ST INNER JOIN STAGEESCALATORS SE ON ST.StageID = SE.StageID
+	INNER JOIN DEPARTMENT DT ON SE.DepartmentID = DT.DepartmentID 
 	WHERE ST.WorkflowID = @WorkflowID
 
 END
 GO
-/****** Object:  StoredProcedure [dbo].[usp_GetWorkflowMeta]    Script Date: 15-01-2022 01:52:51 ******/
+/****** Object:  StoredProcedure [dbo].[usp_GetWorkflowMeta]    Script Date: 15-01-2022 12:07:23 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -54,6 +58,6 @@ CREATE PROCEDURE [dbo].[usp_GetWorkflowMeta]
 AS
 BEGIN
     SET NOCOUNT ON
-	SELECT * FROM Workflow WHERE (@WorkflowID IS NULL OR WorkflowID = @WorkflowID)
+	SELECT WorkflowID, Title, Details FROM Workflow WHERE (@WorkflowID IS NULL OR WorkflowID = @WorkflowID)
 END
 GO
