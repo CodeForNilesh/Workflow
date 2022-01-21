425d58f15c95bf9aac7b5939444a16f3f9cd59f5
CodeForNilesh <patilnileshg@gmail.com>
2022-01-21 09:09:43 +0530

    Request Workflow
    1] Added Reqest controller with functions -
        CreateWorkflowRequest
        GetRequests
        ApproveStage
        RejectStage
    2] Added stored procedures -
    usp_CreateWorkflowRequest
    usp_GetRequests
    usp_ApproveStage
    usp_RejectStage

---
+++ Workflow.Request/Entities/WorkflowRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workflow.Request.Entities
+{
+    public class WorkflowRequest
+    {
+        public int RequestID { get; set; }
+        public int WorkflowID { get; set; }
+        public int CurrentStageID { get; set; }
+        public string Desciption { get; set; }
+        public string WorkflowInstance { get; set; }
+
+        public List<RequestStage> RequestStages;
+    }
+
+    public class RequestStage
+    {
+        public int RequestStageID { get; set; }
+        public int RequestID { get; set; }
+        public int StageID { get; set; }
+        public int StageOrder { get; set; }
+        public bool IsAnyApprover { get; set; }
+        public bool IsStageApprovalMandetory { get; set; }
+        public bool ActionCommentMandetory { get; set; }
+
+        public List<RequestStageReviewer> Reviewers;
+    }
+    public class RequestStageReviewer
+    {
+        public int RequestStageReviewerID { get; set; }
+        public int RequestStageID { get; set; }
+        public int ReviewerID { get; set; }
+        public int ReviewTime { get; set; }
+        public bool Approval { get; set; }
+        public string Comment { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ Workflow.Request/RequestController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Workflow.Request.Entities;
+using WorkFlow.DAL;
+
+namespace Workflow.Request
+{
+    public class RequestController
+    {
+        DBManager dbMAnager;
+        public RequestController()
+        {
+            dbMAnager = new DBManager("sqldbConnection");
+        }
+        public void CreateWorkflowRequest(int WorkflowID, string Desciption, string WorkflowInstance)
+        { 
+            IDbDataParameter[] parameters = new IDbDataParameter[]
+            {
+                dbMAnager.CreateParameter("@WorkflowID",         WorkflowID, DbType.Int32),
+                dbMAnager.CreateParameter("@Desciption",         Desciption, DbType.String),
+                dbMAnager.CreateParameter("@WorkflowInstance",   WorkflowInstance, DbType.String)
+            };
+            dbMAnager.Update("usp_CreateWorkflowRequest", CommandType.StoredProcedure, parameters);
+        }
+
+        public List<WorkflowRequest> GetRequests(int ReviewerID)
+        {
+            IDbDataParameter[] parameters = new IDbDataParameter[]
+            {
+                dbMAnager.CreateParameter("@ReviewerID",         ReviewerID, DbType.Int32),
+
+            };
+            DataTable dt = dbMAnager.GetDataTable("usp_GetRequests", CommandType.StoredProcedure, parameters);
+
+            List<WorkflowRequest> WorkflowRequests = new List<WorkflowRequest>();
+            if (dt?.Rows.Count > 0)
+            {
+                WorkflowRequests = (from c in dt.AsEnumerable()
+                              select new WorkflowRequest
+                              {
+                                  RequestID = c.Field<int>("RequestID"),
+                                  WorkflowID = c.Field<int>("WorkflowID"),
+                                  CurrentStageID = c.Field<int>("CurrentStageID"),
+                                  Desciption = c.Field<string>("Desciption")
+                              }).ToList();
+            }
+            return WorkflowRequests;
+        }
+
+        public void ApproveStage(int RequestStageID, int ReviewerID)
+        {
+
+            IDbDataParameter[] parameters = new IDbDataParameter[]
+            {
+                dbMAnager.CreateParameter("@RequestStageID",     RequestStageID, DbType.Int32),
+                dbMAnager.CreateParameter("@ReviewerID",         ReviewerID, DbType.Int32)
+            };
+            dbMAnager.Update("usp_ApproveStage", CommandType.StoredProcedure, parameters);
+        }
+        public void RejectStage(int RequestStageID, int ReviewerID)
+        {
+
+            IDbDataParameter[] parameters = new IDbDataParameter[]
+            {
+                dbMAnager.CreateParameter("@RequestStageID",     RequestStageID, DbType.Int32),
+                dbMAnager.CreateParameter("@ReviewerID",         ReviewerID, DbType.Int32)
+            };
+            dbMAnager.Update("usp_RejectStage", CommandType.StoredProcedure, parameters);
+        }
+    }
+
+
+}
